b3ac6f427ed664913e5871ff404f49d73d27dd79
gitwes <wesleyfreire1707@gmail.com>
2024-03-14 13:12:27 -0300

    Importando as bibliotecas

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 #Adicionando as biblioteclas
 # pip install selenium openpyxl
+
+#Imports
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import openpyxl
+
+#Acessar o site https://www.novaliderinformatica.com.br/computadores
+driver = webdriver.Chrome()
+driver.get()
